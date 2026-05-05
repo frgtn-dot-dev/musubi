aac1ae3f1f7c4d27162987240fbe50ab9cf5bbd9
Filip Tůma <flamendrin@gmail.com>
2026-05-05 21:03:07 +0200

    Honestly trying why the build is not comming through...

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -7,28 +7,28 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-      starlight({
-          title: 'Musubi',
-          customCss: ['./src/styles/custom.css'],
-          sidebar: [
-              {
-                  label: 'Getting Started',
-                  items: [
-                      { label: 'Introduction', slug: 'guides/introduction' },
-                      { label: 'Running Locally', slug: 'guides/running-locally' },
-                  ],
-              },
-              {
-                  label: 'App',
-                  items: [
-                      { label: 'Client', slug: 'reference/client' },
-                      { label: 'Server', slug: 'reference/server' },
-                      { label: 'Database Schema', slug: 'reference/schema' },
-                  ],
-              },
+    starlight({
+      title: 'Musubi',
+      customCss: ['./src/styles/custom.css'],
+      sidebar: [
+        {
+          label: 'Getting Started',
+          items: [
+            { label: 'Introduction', slug: 'guides/introduction' },
+            { label: 'Running Locally', slug: 'guides/running-locally' },
           ],
-      }),
-	],
+        },
+        {
+          label: 'App',
+          items: [
+            { label: 'Client', slug: 'reference/client' },
+            { label: 'Server', slug: 'reference/server' },
+            { label: 'Database Schema', slug: 'reference/schema' },
+          ],
+        },
+      ],
+    }),
+  ],
 
   adapter: node({
     mode: 'standalone',
