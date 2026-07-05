8947bea62b1ef1a5e4d3ef2a71715114bd0f2e02
Filip Tůma <flamendrin@gmail.com>
2026-07-05 20:11:14 +0200

    Version Bump to 0.0.10

--- apps/client/app.config.ts
+++ apps/client/app.config.ts
@@ -2,7 +2,7 @@ const expoConfig = {
   name: "Musubi",
   slug: "musubi",
   owner: "frgtn",
-  version: "0.0.9",
+  version: "0.0.10",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "musubi",
