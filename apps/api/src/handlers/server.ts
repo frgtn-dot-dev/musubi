93cec7914dcea83d9f62546aaf3a2da533cdbb8d
Filip Tůma <flamendrin@gmail.com>
2026-07-06 19:48:43 +0200

    Bump Server Requested Verison to 0.0.11

--- apps/api/src/handlers/server.ts
+++ apps/api/src/handlers/server.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 
 
-export function handlerServerStatus(req: Request, res: Response) {
+export function handlerServerStatus(_: Request, res: Response) {
   res.status(200).json({ ok: true });
 }
 
-export function handlerServer(req: Request, res: Response) {
-  res.status(200).json({ minClientVersion: "0.0.9" });
+export function handlerServer(_: Request, res: Response) {
+  res.status(200).json({ minClientVersion: "0.0.11" });
 }
 
